a3a1db4270818669aea021c2e5a6a5f477ee5ec2
Burt Beckwith <beckwithb@vmware.com>
2012-08-20 00:00:46 -0400

    update to latest release plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -25,7 +25,7 @@ grails.project.dependency.resolution = {
 	plugins {
 		compile ':spring-security-core:1.2.7.3'
 
-		build(':release:2.0.3', ':rest-client-builder:1.0.2') {
+		build(':release:2.0.4', ':rest-client-builder:1.0.2') {
 			export = false
 		}
 	}
