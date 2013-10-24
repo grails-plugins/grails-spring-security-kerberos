c0779538f018c876a8b54d29706cbd16d116623b
Burt Beckwith <burt@burtbeckwith.com>
2013-10-24 13:09:33 -0400

    changed package to be consistent with the core plugin

--- src/groovy/grails/plugin/springsecurity/kerberos/NoopTicketValidator.groovy
+++ src/groovy/grails/plugin/springsecurity/kerberos/NoopTicketValidator.groovy
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.codehaus.groovy.grails.plugins.springsecurity.kerberos
+package grails.plugin.springsecurity.kerberos
 
 import org.springframework.security.extensions.kerberos.KerberosTicketValidator
 
