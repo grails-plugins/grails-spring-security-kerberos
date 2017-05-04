11bff729f75a4d07c4eb533d920de5c19ec34449
sdelamo <sergio.delamo@softamo.com>
2017-05-04 14:06:54 +0200

    Add travis configuration

---
+++ travis-build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+rm -rf build
+
+./gradlew -q clean check install --stacktrace
+
+cd functional-test-app
+
+./gradlew check
+
+cd ..
+
+if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+    
+    echo "In master no pullrequest - tag: $TRAVIS_TAG"
+
+    if [[ -n $TRAVIS_TAG ]]; then
+
+        ./gradlew bintrayUpload --stacktrace
+
+        ./publish-docs.sh
+
+    fi
+
+fi
